f420160ce3b0617f07715a3b77d206b61283003c
George & Petru <George & Petru@GEORGE_PETRU>
2022-08-30 04:44:16 -0700

    Add project files.

---
+++ Rabbitmq/ProducerConsumerRabitMQ/Producer/Sender.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    internal class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var chanel = connection.CreateModel())
+            {
+                chanel.QueueDeclare("BasicTest", false, false, false, null);
+                string message = "Getting started with .Net Core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                chanel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine($"Sent message {message}");
+            }
+
+            Console.WriteLine("Press [enter] to exit");
+            Console.ReadLine();
+        }
+    }
+}
