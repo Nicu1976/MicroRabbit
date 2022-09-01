fa8a7d3030abcd78c3ee1dc3c47a14a34b652d38
Stoian Nicolae <stoiand_nicu@yahoo.com>
2022-09-01 03:54:56 -0700

    Partially completed Banking Microservice

--- MicroRabbit/MicroRabbit.Banking.Api/Startup.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Startup.cs
@@ -1,7 +1,11 @@
+using MediatR;
+using MicroRabbit.Banking.Application.Context;
+using MicroRabbit.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +30,26 @@ namespace MicroRabbit.Banking.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<BankingDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection"));
+            }
+            );
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Banking Microservice", Version = "v1" });
+            });
+
+            services.AddMediatR(typeof(Startup));
+
+            RegisterServices(services);
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +61,9 @@ namespace MicroRabbit.Banking.Api
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1"));
 
             app.UseRouting();
 
---
+++ MicroRabbit/MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,27 @@
+﻿using MicroRabbit.Banking.Application.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Application.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private BankingDbContext _ctx;
+
+
+        public AccountRepository(BankingDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _ctx.Accounts;
+        }
+    }
+}
